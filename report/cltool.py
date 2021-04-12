8ae77df2c418d5b946a918fe866a576d2563a265
Thomas <thomas.golding@gmail.com>
2021-04-12 11:47:37 +0200

    script not working...

---
+++ report/cltool.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import argparse
+
+from report.write_report import generate_report
+
+    
+def cltool():
+    parser = argparse.ArgumentParser(description='Duplicate file report.')
+    parser.add_argument("rootdir", help="The rootdir for which to generate the report.")
+    args = parser.parse_args()
+
+    p = Path(args.rootdir)
+    if p.is_dir() and p.exists():
+        rep = generate_report(path=p)
+        print(rep)
+    else:
+        print("not valid rootdir.")
+    
+
+
+if __name__ == __name__:
+    cltool()
--- report/write_report.py
+++ report/write_report.py
@@ -1,7 +1,23 @@
 
+import argparse
+from pathlib import Path
+
 
 import pandas as pd
 
-from dirtree.dirtree import DirTree
+from hash_tree.hash_tree import HashTree
 
 
+
+def generate_report(path: Path):
+    hashtree = HashTree(rootpath=path)
+    tree = hashtree.get_tree()
+    df = pd.DataFrame.from_records(tree)
+    hashcol = df["hash"].value_counts()
+    # hashcol=hashcol[hashcol>1]
+    hashcol.name = "duplicates"
+    res = df.merge(hashcol, left_on="hash", right_index=True)
+    res.sort_values(by=["duplicates", "hash"], ascending=False, inplace=True)
+    return res
+    
+    
