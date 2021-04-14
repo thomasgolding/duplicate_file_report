b58f9c4abeceeffbc8804f56a499f615d7f9322f
Thomas <thomas.golding@gmail.com>
2021-04-14 23:45:29 +0200

    Added target and removed the __name__ is __main__ double run.

--- duplicate_file_report/cltool.py
+++ duplicate_file_report/cltool.py
@@ -5,18 +5,22 @@ from duplicate_file_report.write_report import generate_report
 
     
 def cltool():
-    parser = argparse.ArgumentParser(description='Duplicate file report.')
+    parser = argparse.ArgumentParser(description="Duplicate file report.")
     parser.add_argument("rootdir", help="The rootdir for which to generate the report.")
+    parser.add_argument("target", help="File where report.")
     args = parser.parse_args()
 
     p = Path(args.rootdir)
+    t = Path(args.target)
+    if t.exists():
+        print("Target must not exist.")
+        return
     if p.is_dir() and p.exists():
         rep = generate_report(path=p)
-        print(rep)
+        rep.to_csv(t)
+        print(f"Report written in {t.name}")
     else:
         print("not valid rootdir.")
     
 
 
-if __name__ == __name__:
-    cltool()
